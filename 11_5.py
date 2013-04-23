9c07ee2241bfa38bac5764417fc97f250a83fe4f
Nitin Punjabi <nptoronto@yahoo.com>
2013-04-23 10:15:00 +0200

    Solve Ex 11.5

---
+++ 11_5.py
@@ -0,0 +1,46 @@
+# 11.5 Given a sorted array of strings which is interspersed with empty strings,
+# write a method to find the location of a given string.
+# Example
+# Input: find "ball" in ["at", "", "", "", "ball", "", "", "car", "", "",
+#                        "dad", "", ""]
+# output: 4
+
+def _find_string(elems, left, right, target):
+  if left > right:
+    return False
+
+  m = (left + right) / 2
+  if elems[m] is target:
+    return m
+
+  if elems[m]:
+    if elems[m] > target:
+      return _find_string(elems, left, m - 1, target)
+    else:
+      return _find_string(elems, m + 1, right, target)
+  else:
+    left_str = m
+    right_str = m
+    while left_str >= 0 and not elems[left_str]:
+      left_str -= 1
+    while right_str <= right and not elems[right_str]:
+      right_str += 1
+
+    if elems[left_str]:
+      if elems[left_str] is target:
+        return left_str
+      elif elems[left_str] > target:
+        return _find_string(elems, left, left_str - 1, target)
+    
+    if elems[right_str]:
+      if elems[right_str] is target:
+        return right_str
+      elif elems[right_str] < target:
+        return _find_string(elems, right_str + 1, right, target)
+
+  return False
+
+def find_string(elems, target):
+  return _find_string(elems, 0, len(elems) - 1, target)
+
+ws = ["at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""]
